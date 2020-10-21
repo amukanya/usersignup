844110696826e0e0596d06f9ef0bcc92c25d899e
Andy Mukanya <mukanya56@gmail.com>
2020-10-20 20:33:50 -0500

    Added validation to the welcome page

--- main.py
+++ main.py
@@ -8,33 +8,54 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
+    at = '@'
+    point = '.'
     username = request.form['username']
     password = request.form['password']
     confpassword = request.form['confpassword']
-    name_error = ''
-    pass_error = ''
-    confpass_error = ''
-    if username == '':
+    email = request.form['email']
+    name_error =''
+    pass_error =''
+    confpass_error =''
+    email_error =''
+    if username =='':
         name_error = 'You did not enter a username.'
-        username = ''
-    if len(username) <= 3:
+        username =''
+    elif len(username) <= 3:
         name_error = 'Username must have more than 3 letters.'
-        username = ''
-    if password == '':
-        pass_error = 'You did not enter a password.'
-        password = ''
-    else:
-        if not confpassword == password:
-            confpass_error = 'Make sure Confirmatiom password matches password.'
-            confpassword = ''
+        username =''
     
-    if not name_error and not confpass_error:
+    if password =='':
+        pass_error = 'You did not enter a password.'
+        password =''
+    elif len(password) <= 3:
+        name_error = 'Username must have more than 3 letters.'
+        username =''
+    else:
+        if confpassword != password:
+            confpass_error = 'Make sure Confirmatiom password matches password.'
+            confpassword =''
+    if email !='':
+        if len(email) < 3:
+            email_error = 'Your email is too short.'
+            email =''
+        elif len(email) > 20:
+            email_error = 'Your email is too long.'
+        else:
+            if not at in email:
+                email_error = 'You are missing -@- .'
+                email =''
+            elif not point in email:
+                email_error = 'You are missing -.- .'
+                email =''
+    
+    if (not name_error and not confpass_error) and (not pass_error and not email_error):
         username_bold = "<strong>" + username + "</strong>"
         sentence = "Welcome, " + username_bold
         content = "<p>" + sentence + "</p>"
         return content
     else:
-        return render_template('edut.html', name_error = name_error, pass_error = pass_error, confpass_error = confpass_error, username = username, password = password, confpassword = confpassword)
+        return render_template('edit.html', name_error = name_error, pass_error = pass_error, confpass_error = confpass_error,email_error = email_error, username = username, password = password, confpassword = confpassword, email = email)
 
 @app.route("/")
 def index():
